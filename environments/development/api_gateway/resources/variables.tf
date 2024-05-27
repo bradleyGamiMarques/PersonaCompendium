379597f34e0b7cc1a7bda2898c67d6dc1543740d
bradleyGamiMarques <bradleygamimarques@outlook.com>
2024-05-26 18:39:17 -0700

    [feature] Add api_gateway resources module

---
+++ environments/development/api_gateway/resources/variables.tf
@@ -0,0 +1,9 @@
+variable "rest_api_id" {
+  description = "Rest api id of api gateway"
+  type        = string
+}
+
+variable "root_resource_id" {
+  description = "Root resource id of api gateway"
+  type        = string
+}
