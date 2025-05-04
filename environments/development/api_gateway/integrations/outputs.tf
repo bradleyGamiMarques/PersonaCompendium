796613ad7b2038874fe99f3d19a5f68ae311610d
Bradley Marques <bradleygamimarques@gmail.com>
2025-05-04 02:02:05 -0700

    Refactor: remove terraform implementation
    This is a major change to the project. I have decided to change direction and make use of AWS SAM for building out my serverless functions/apis

--- environments/development/api_gateway/integrations/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "v1_get_p3r_persona_by_name_lambda_integration" {
-  value = aws_api_gateway_integration.v1_get_p3r_persona_by_name_lambda_integration
-}
-
-output "v1_get_p3r_personas_by_arcana_lambda_integration" {
-  value = aws_api_gateway_integration.v1_get_p3r_personas_by_arcana_lambda_integration
-}
